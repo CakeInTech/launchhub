7187837d328243469d93e75c461a0c20cfa75941
CakeInTech <abdulhakimm280@gmail.com>
2024-11-15 11:15:39 +0200

    Install & setup sanity

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,15 @@
+import {defineQuery} from "next-sanity";
+
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+  _id, 
+    title, 
+    slug,
+    _createdAt, 
+    views, 
+    description, 
+    image, 
+    category, 
+    author -> {
+      _id, name, image, bio
+    }, 
+}`)
