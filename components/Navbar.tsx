2076bee77959e350ad5e1fd86b884698635d9f9f
CakeInTech <abdulhakimm280@gmail.com>
2024-12-13 23:53:34 +0200

    get user data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Header = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={120} height={100} />
+          <Image src="/logo.png" alt="Logo" width={144} height={30} />
         </Link>
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
